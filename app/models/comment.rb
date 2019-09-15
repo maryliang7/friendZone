9c0e738c4c90f585ffcbe6a0bfa62c70310419db
Mary Liang <mary.liang96@gmail.com>
2019-09-15 00:24:33 -0700

    fixed comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -9,12 +9,13 @@ class Comment < ApplicationRecord
     foreign_key: :author_id,
     class_name: :User
 
-  belongs_to :parent
+  belongs_to :parent,
     foreign_key: :parent_id,
     class_name: :Comment,
     optional: true
 
   has_many :children,
     foreign_key: :parent_id,
-    class_name: :Comment
+    class_name: :Comment,
+    optional: true
 end
